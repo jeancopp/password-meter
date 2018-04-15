1ebac8951433292c238101e639142101de37597b
Jean Coppieters <jean@coppieters.com.br>
2018-04-15 17:23:48 -0300

    Lista implementada

--- src/main/java/estruturas/lista/ExemploLista.java
+++ src/main/java/estruturas/lista/ExemploLista.java
@@ -3,19 +3,44 @@ package estruturas.lista;
 public class ExemploLista {
 	
 	public static void main(String[] args) {
-		
+		/* 
+		 * A Lista talvez seja a estrutura de dados mais utilizada normalmente, 
+		 * por permitir que trabalhar com uma grande quantidade de dados.
+		 * O limite dela é o tamanho de memória que houver disponível, havendo 
+		 * a possíbilidade de, dada uma implementação, usar também a memória em disco.
+		 * Diferente d'uma estrutura clássica de vetor, onde o tamanho deverá ser préviamente 
+		 * definido, a lista permitirá adicionarmos a quantidade de elementos necessários. 
+		 * Porém, diferente do vetor clássico, é preciso percorrê-lá para acessar os elementos,
+		 * já que não obrigatóriamente os elementos estarão dispostos em sequência na memória.
+		 * Há implementações que há tratativas diferentes, obviamente, mas a idéia seguirá próxima.
+		 * 
+		 * Na implementação abaixo, optou-se por usar um lista duplamente encadeada, de forma 
+		 * que cada elemento tem acesso rápido ao seguinte e ao anterior.
+		 * Essa é uma outra desvantagem, já que para armazenar um elemento é preciso dispor do 
+		 * espaço de memória do mesmo + referência ao anterior + referencia ao próximo, 
+		 * bem como outras referências usadas pela lista, como um ponteiro para o primeiro e para o último elemento.
+		 * 
+		 * Ou seja, o gasto é quantidade de elementos * (tamanho de memório preciso + duas referencias) + duas referencias
+		 *  
+		 */
 		Lista lista = new Lista();
-		for(int i = 0; i < 10; i++)
+		for(int i = 0; i < 10; i++){
+			System.out.println("Adicionando o item("+i+") à lista");
 			lista.adicionar(new Integer(i));
+			lista.mostrarOPrimeiro();
+			lista.mostrarOUltimo();
+			System.out.println("Tamanho atual:"+lista.getTamanho());
+		}
 		
 		int elemento = lista.retornar(5);
-		System.out.println(elemento);
+		System.out.println("Elemento no indice 5: " + elemento);
 		
 		int elementoRemovido = lista.remover(5);
-		System.out.println(elementoRemovido);
+		System.out.println("Elemento do indice 5 foi removido:" +elementoRemovido);
 		
 		int novoElementoNoIndice5 = lista.retornar(5);
-		System.out.println(novoElementoNoIndice5);
+		System.out.println("Novo elemento do indice 5 é: " + novoElementoNoIndice5);
+		System.out.println("Tamanho atual:"+lista.getTamanho());
 		
 	}
 	
--- src/main/java/estruturas/lista/Lista.java
+++ src/main/java/estruturas/lista/Lista.java
@@ -14,30 +14,61 @@ public class Lista {
 	
 	public void adicionar(Integer valor) {
 		Elemento novo = new Elemento(valor);
-		
-		if(fim == null) fim = novo;
-		
-		fim.setProximo(novo);
+		if(this.emBranco()){
+			comeco = novo;
+			fim = novo;
+		}
 		novo.setAnterior(fim);
+		fim.setProximo(novo);
 		
-		if(comeco == null) comeco = novo;
-		
-		fim = novo;
+		this.fim = novo;
 		tamanho ++;
 	}
 	
 	
 	
 	public Integer retornar(int indiceDoElemento) {
+		Elemento elementoBuscado = buscarElemento(indiceDoElemento);
+		return (null == elementoBuscado) ? null : elementoBuscado.getValor();
+	}
+
+	private Elemento buscarElemento(int indiceDoElemento) {
 		if( tamanho < indiceDoElemento ) return null;
 		Elemento elementoBuscado = comeco;
 		for(int i = 0; i < indiceDoElemento; i++) elementoBuscado = elementoBuscado.getProximo();
+		return elementoBuscado;
+	}
+
+	public Integer remover(int indiceDoElemento) {
+		Elemento elementoBuscado = buscarElemento(indiceDoElemento);
+		if(null == elementoBuscado) return null;
+		
+		Elemento anterior = elementoBuscado.getAnterior();
+		Elemento proximo = elementoBuscado.getProximo();
+		
+		anterior.setProximo(proximo);
+		proximo.setAnterior(anterior);
+		
+		elementoBuscado.setAnterior(null);
+		elementoBuscado.setProximo(null);
+		tamanho--;
 		
 		return elementoBuscado.getValor();
 	}
 
-	public Integer remover(int indiceDoElemento) {
-		return 0;
+	public void mostrarOPrimeiro() {
+		System.out.println( this.emBranco() ? "Não há elementos na lista" : "Primeiro elemento:"+comeco.getValor());
+	}
+	private boolean emBranco() {
+		return this.tamanho == 0;
+	}
+
+	public void mostrarOUltimo() {
+		System.out.println( this.emBranco() ? "Não há elementos na lista" : "Ultimo elemento:"+fim.getValor());
+	}
+
+	public Integer getTamanho() {
+		return tamanho;
 	}
 	
 }
